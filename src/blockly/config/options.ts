32ee6ac0aad7bbfe681d7a93bbc64691d05f9497
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2025-02-06 10:07:00 -0300

    feat: Melhoria nas opções do Blockly
    As opções de configuração do Blockly foram expandidas e reorganizadas para melhor legibilidade e manutenção. Agora, as opções estão divididas em categorias separadas como grid, move, toolbox e zoom. Além disso, novas funcionalidades foram adicionadas como a capacidade de colapsar blocos, habilitar comentários, definir o número máximo de itens excluídos na lixeira e muito mais.

--- src/blockly/config/options.ts
+++ src/blockly/config/options.ts
@@ -1,24 +1,60 @@
 import { BlocklyOptions } from 'blockly';
 
-const blocklyOptions: BlocklyOptions = {
-  grid: { spacing: 20, length: 3, colour: '#ccc', snap: true },
-  maxBlocks: Infinity,
-  sounds: true,
-  toolbox: {
-    kind: 'flyoutToolbox',
-    contents: [
-      {
-        kind: 'block',
-        type: 'controls_if',
-      },
-      {
-        kind: 'block',
-        type: 'controls_whileUntil',
-      },
-      // You can add more blocks to this array.
-    ],
+const grid = {
+  spacing: 20,
+  length: 3,
+  colour: '#ccc',
+  snap: true,
+};
+
+const move = {
+  scrollbars: {
+    horizontal: true,
+    vertical: true,
   },
-  trashcan: true,
+  drag: true,
+  wheel: false,
+};
+
+const toolbox = {
+  kind: 'flyoutToolbox',
+  contents: [
+    {
+      kind: 'block',
+      type: 'controls_if',
+    },
+    {
+      kind: 'block',
+      type: 'controls_whileUntil',
+    },
+  ],
+};
+
+const zoom = {
+  controls: true,
+  wheel: false,
+  startScale: 1.0,
+  maxScale: 3,
+  minScale: 0.3,
+  scaleSpeed: 1.2,
+  pinch: true,
+};
+
+const blocklyOptions: BlocklyOptions = {
+  collapse: true, // Allow the user to collapse blocks.
+  comments: true, // Enable comments.
+  grid, // Enable grid and snap to grid.
+  maxBlocks: Infinity, // Max blocks in workspace.
+  maxTrashcanContents: 32, // The maximum number of deleted items to store in the trashcan.
+  move,
+  readOnly: false, // Enable read-only mode.
+  renderer: 'zelos', // Os renderizadores pré-empacotados incluem "geras" (o padrão), "thrasos" e "zelos" (um renderizador semelhante a scratch)
+  scrollbars: true, // Enable horizontal and vertical scrollbars.
+  sounds: true, // Enable sounds.
+  toolbox,
+  toolboxPosition: 'start', // Position of the toolbox.
+  trashcan: true, // Enable the trashcan.
+  zoom,
 };
 
 export default blocklyOptions;
