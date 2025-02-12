1e13156e98f6257835d3c9648df60132dbae3376
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2025-02-12 14:12:25 -0300

    feat: Atualização dos blocos de construção
    Os blocos de construção foram atualizados para incluir mais detalhes e funcionalidades. Agora, cada bloco contém um campo adicional que especifica o tipo e o texto. Além disso, novas conexões foram adicionadas a alguns blocos, expandindo suas possibilidades de uso.
    
    As mudanças significativas incluem:
    - Adição do campo 'fields' em todos os blocos
    - Expansão das opções de conexão no bloco 'test5'
    - Criação dos novos blocos 'test6' e 'test7', com campos dropdown e checkbox respectivamente

--- src/blockly/blocks/index.ts
+++ src/blockly/blocks/index.ts
@@ -11,7 +11,10 @@ const configCustomBlocks = () => {
       hasNextConnection: 'goiabinha',
       helpUrl: 'http://www.test.com',
       name: 'test',
-      text: 'test',
+      fields: [{
+        type: 'text',
+        text: 'test',
+      }],
       tooltip: 'this is a test',
     }),
     blockConstructor({
@@ -20,7 +23,10 @@ const configCustomBlocks = () => {
       hasNextConnection: 'feijão',
       helpUrl: 'http://www.test.com',
       name: 'test2',
-      text: 'test2',
+      fields: [{
+        type: 'text',
+        text: 'test2',
+      }],
       tooltip: 'this is a test 2',
     }),
     blockConstructor({
@@ -29,7 +35,10 @@ const configCustomBlocks = () => {
       hasNextConnection: 'sorvete',
       helpUrl: 'http://www.test.com',
       name: 'test3',
-      text: 'test3',
+      fields: [{
+        type: 'text',
+        text: 'test3',
+      }],
       tooltip: 'this is a test 3',
     }),
     blockConstructor({
@@ -37,17 +46,78 @@ const configCustomBlocks = () => {
       hasOutput: 'Panela',
       helpUrl: 'http://www.test.com',
       name: 'test4',
-      text: 'test4',
+      fields: [{
+        type: 'text',
+        text: 'test4',
+      }],
       tooltip: 'this is a test 4',
     }),
     blockConstructor({
       colour: 1,
       hasPreviousConnection: ['goiabinha', 'feijão', 'sorvete'],
-      hasNextConnection: ['goiabinha', 'feijão', 'sorvete'],
+      hasNextConnection: ['goiabinha', 'feijão', 'sorvete', 'mamão'],
       helpUrl: 'http://www.test.com',
       name: 'test5',
-      text: 'test5',
+      fields: [
+        {
+          type: 'text',
+          text: 'var test5 = %1 \n to %2',
+        },
+        {
+          type: 'field_variable',
+          name: 'VAR',
+          variable: 'testFive',
+          variableTypes: [''],
+        },
+        {
+          type: 'input_value',
+          name: 'TO',
+        },
+      ],
       tooltip: 'this is a test 5',
+    }),
+    blockConstructor({
+      colour: 250,
+      hasPreviousConnection: 'mamão',
+      hasNextConnection: 'Panela',
+      helpUrl: 'http://www.test.com',
+      name: 'test6',
+      fields: [
+        {
+          type: 'text',
+          text: 'drop down: %1',
+        },
+        {
+          type: 'field_dropdown',
+          name: 'DROPDOWN',
+          options: [
+            ['qwerty', 'qwerty'],
+            ['qwerty2', 'qwerty2'],
+            ['qwerty3', 'qwerty3'],
+            ['qwerty4', 'qwerty4'],
+          ],
+        },
+      ],
+      tooltip: 'this is a test 6',
+    }),
+    blockConstructor({
+      colour: 250,
+      hasPreviousConnection: 'Panela',
+      hasNextConnection: null,
+      helpUrl: 'http://www.test.com',
+      name: 'test7',
+      fields: [
+        {
+          type: 'text',
+          text: 'checkbox: %1',
+        },
+        {
+          type: 'field_checkbox',
+          name: 'CHECKBOX',
+          checked: true,
+        },
+      ],
+      tooltip: 'this is a test 7',
     }),
   ];
 };
