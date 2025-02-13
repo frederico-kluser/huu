ab0950fe10a93d3526254d5579ae59475fe831c2
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2025-02-13 09:28:07 -0300

    feat: Adiciona bloco para recarregar página
    Um novo bloco foi adicionado à categoria de controle de URL. Este bloco permite que os usuários recarreguem a página atual. Além disso, o arquivo index da categoria de controle de URL foi atualizado para incluir este novo bloco.

---
+++ src/blockly/blocks/3-category_url-control/blocks/BlockRefreshPage.ts
@@ -0,0 +1,25 @@
+import * as Blockly from 'blockly/core';
+import Colors from '../../../config/colors';
+import blockConstructor from '../../../helpers/blockConstructor';
+
+const setBlockRefreshPage = () => {
+    return blockConstructor({
+        colour: Colors.URL, // utilize a cor desejada para este bloco
+        hasPreviousConnection: null,
+        hasNextConnection: null,
+        helpUrl: 'https://developer.mozilla.org/en-US/docs/Web/API/Location/reload',
+        name: 'BlockRefreshPage',
+        fields: [
+            {
+                type: 'text',
+                text: 'recarregar página'
+            }
+        ],
+        tooltip: 'Recarrega a página atual.',
+        generator: function (block: Blockly.Block, generator: any) {
+            return 'window.location.reload();\n';
+        }
+    });
+};
+
+export default setBlockRefreshPage;
--- src/blockly/blocks/3-category_url-control/index.ts
+++ src/blockly/blocks/3-category_url-control/index.ts
@@ -1,11 +1,13 @@
 import { TypeBlockly } from "../../helpers/blockConstructor";
 import setBlockNavigateToUrlText from "./blocks/BlockNavigateToUrlText";
 import setBlockNavigateToUrlVariable from "./blocks/BlockNavigateToUrlVariable";
+import setBlockRefreshPage from "./blocks/BlockRefreshPage";
 
 const configURLActions = (): TypeBlockly[] => {
     return [
         setBlockNavigateToUrlVariable(),
         setBlockNavigateToUrlText(),
+        setBlockRefreshPage(),
     ];
 };
 
