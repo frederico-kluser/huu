4660992ac2c45009a2fcc1c6ebd583afe5d1a5f2
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2025-03-04 12:45:25 -0300

    refactor: Alteração na estrutura de remoção de atalhos
    A estrutura para remover atalhos foi modificada. Agora, em vez de atribuir um único ouvinte de evento a cada atalho, estamos empilhando-os em uma matriz. Isso permite que vários ouvintes sejam registrados para o mesmo atalho, aumentando a flexibilidade do nosso sistema de atalhos.

--- src/pages/Content/index.ts
+++ src/pages/Content/index.ts
@@ -5,7 +5,7 @@ console.log('Content script works!');
 
 const removeShortcuts: {
     [key: string]: {
-        removeListener: () => void;
+        removeListener: Array<() => void>;
         lastUpdate: number;
     };
 } = {};
@@ -20,38 +20,40 @@ getTabAgents(window.location.href).then((agents) => {
             eval(agent.code);
         };
 
+        removeShortcuts[agent.name].removeListener = [];
+
         switch (agent.mode) {
             case 'manual-shortcut-2':
-                removeShortcuts[agent.name].removeListener = registerShortcut([ValidKey.ControlLeft, ValidKey.Digit2], evalCode);
-                removeShortcuts[agent.name].removeListener = registerShortcut([ValidKey.ControlRight, ValidKey.Digit2], evalCode);
+                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlLeft, ValidKey.Digit2], evalCode));
+                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlRight, ValidKey.Digit2], evalCode));
                 break;
             case 'manual-shortcut-3':
-                removeShortcuts[agent.name].removeListener = registerShortcut([ValidKey.ControlLeft, ValidKey.Digit3], evalCode);
-                removeShortcuts[agent.name].removeListener = registerShortcut([ValidKey.ControlRight, ValidKey.Digit3], evalCode);
+                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlLeft, ValidKey.Digit3], evalCode));
+                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlRight, ValidKey.Digit3], evalCode));
                 break;
             case 'manual-shortcut-4':
-                removeShortcuts[agent.name].removeListener = registerShortcut([ValidKey.ControlLeft, ValidKey.Digit4], evalCode);
-                removeShortcuts[agent.name].removeListener = registerShortcut([ValidKey.ControlRight, ValidKey.Digit4], evalCode);
+                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlLeft, ValidKey.Digit4], evalCode));
+                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlRight, ValidKey.Digit4], evalCode));
                 break;
             case 'manual-shortcut-5':
-                removeShortcuts[agent.name].removeListener = registerShortcut([ValidKey.ControlLeft, ValidKey.Digit5], evalCode);
-                removeShortcuts[agent.name].removeListener = registerShortcut([ValidKey.ControlRight, ValidKey.Digit5], evalCode);
+                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlLeft, ValidKey.Digit5], evalCode));
+                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlRight, ValidKey.Digit5], evalCode));
                 break;
             case 'manual-shortcut-6':
-                removeShortcuts[agent.name].removeListener = registerShortcut([ValidKey.ControlLeft, ValidKey.Digit6], evalCode);
-                removeShortcuts[agent.name].removeListener = registerShortcut([ValidKey.ControlRight, ValidKey.Digit6], evalCode);
+                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlLeft, ValidKey.Digit6], evalCode));
+                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlRight, ValidKey.Digit6], evalCode));
                 break;
             case 'manual-shortcut-7':
-                removeShortcuts[agent.name].removeListener = registerShortcut([ValidKey.ControlLeft, ValidKey.Digit7], evalCode);
-                removeShortcuts[agent.name].removeListener = registerShortcut([ValidKey.ControlRight, ValidKey.Digit7], evalCode);
+                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlLeft, ValidKey.Digit7], evalCode));
+                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlRight, ValidKey.Digit7], evalCode));
                 break;
             case 'manual-shortcut-8':
-                removeShortcuts[agent.name].removeListener = registerShortcut([ValidKey.ControlLeft, ValidKey.Digit8], evalCode);
-                removeShortcuts[agent.name].removeListener = registerShortcut([ValidKey.ControlRight, ValidKey.Digit8], evalCode);
+                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlLeft, ValidKey.Digit8], evalCode));
+                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlRight, ValidKey.Digit8], evalCode));
                 break;
             case 'manual-shortcut-9':
-                removeShortcuts[agent.name].removeListener = registerShortcut([ValidKey.ControlLeft, ValidKey.Digit9], evalCode);
-                removeShortcuts[agent.name].removeListener = registerShortcut([ValidKey.ControlRight, ValidKey.Digit9], evalCode);
+                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlLeft, ValidKey.Digit9], evalCode));
+                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlRight, ValidKey.Digit9], evalCode));
                 break;
         }
     });
