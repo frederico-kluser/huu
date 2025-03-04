7b58c479acb2e3ad399e97d4409d3e999d91e787
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2025-03-04 12:49:57 -0300

    refactor: Melhoria na gestão de atalhos
    A lógica para gerenciar os atalhos foi encapsulada em uma função separada. Isso facilita a remoção e o registro de novos atalhos, tornando o código mais limpo e fácil de manter. Além disso, foram adicionados novos casos para diferentes modos de atalho.

--- src/pages/Content/index.ts
+++ src/pages/Content/index.ts
@@ -10,57 +10,69 @@ const removeShortcuts: {
     };
 } = {};
 
-getTabAgents(window.location.href).then((agents) => {
-    console.log('agents:', agents);
-
-    agents.forEach((agent) => {
-        if (!agent.active) return;
-
-        const evalCode = () => {
-            eval(agent.code);
-        };
-
-        removeShortcuts[agent.name].removeListener = [];
-
-        switch (agent.mode) {
-            case 'manual-shortcut-2':
-                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlLeft, ValidKey.Digit2], evalCode));
-                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlRight, ValidKey.Digit2], evalCode));
-                break;
-            case 'manual-shortcut-3':
-                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlLeft, ValidKey.Digit3], evalCode));
-                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlRight, ValidKey.Digit3], evalCode));
-                break;
-            case 'manual-shortcut-4':
-                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlLeft, ValidKey.Digit4], evalCode));
-                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlRight, ValidKey.Digit4], evalCode));
-                break;
-            case 'manual-shortcut-5':
-                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlLeft, ValidKey.Digit5], evalCode));
-                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlRight, ValidKey.Digit5], evalCode));
-                break;
-            case 'manual-shortcut-6':
-                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlLeft, ValidKey.Digit6], evalCode));
-                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlRight, ValidKey.Digit6], evalCode));
-                break;
-            case 'manual-shortcut-7':
-                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlLeft, ValidKey.Digit7], evalCode));
-                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlRight, ValidKey.Digit7], evalCode));
-                break;
-            case 'manual-shortcut-8':
-                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlLeft, ValidKey.Digit8], evalCode));
-                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlRight, ValidKey.Digit8], evalCode));
-                break;
-            case 'manual-shortcut-9':
-                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlLeft, ValidKey.Digit9], evalCode));
-                removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlRight, ValidKey.Digit9], evalCode));
-                break;
-        }
+const InsertPageAgents = () => {
+    Object.keys(removeShortcuts).forEach((key) => {
+        removeShortcuts[key].removeListener.forEach((removeListener) => {
+            removeListener();
+        });
+        delete removeShortcuts[key];
     });
-});
+
+    getTabAgents(window.location.href).then((agents) => {
+        console.log('agents:', agents);
+
+        agents.forEach((agent) => {
+            if (!agent.active) return;
+
+            const evalCode = () => {
+                eval(agent.code);
+            };
+
+            removeShortcuts[agent.name].removeListener = [];
+
+            switch (agent.mode) {
+                case 'manual-shortcut-2':
+                    removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlLeft, ValidKey.Digit2], evalCode));
+                    removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlRight, ValidKey.Digit2], evalCode));
+                    break;
+                case 'manual-shortcut-3':
+                    removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlLeft, ValidKey.Digit3], evalCode));
+                    removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlRight, ValidKey.Digit3], evalCode));
+                    break;
+                case 'manual-shortcut-4':
+                    removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlLeft, ValidKey.Digit4], evalCode));
+                    removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlRight, ValidKey.Digit4], evalCode));
+                    break;
+                case 'manual-shortcut-5':
+                    removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlLeft, ValidKey.Digit5], evalCode));
+                    removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlRight, ValidKey.Digit5], evalCode));
+                    break;
+                case 'manual-shortcut-6':
+                    removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlLeft, ValidKey.Digit6], evalCode));
+                    removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlRight, ValidKey.Digit6], evalCode));
+                    break;
+                case 'manual-shortcut-7':
+                    removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlLeft, ValidKey.Digit7], evalCode));
+                    removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlRight, ValidKey.Digit7], evalCode));
+                    break;
+                case 'manual-shortcut-8':
+                    removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlLeft, ValidKey.Digit8], evalCode));
+                    removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlRight, ValidKey.Digit8], evalCode));
+                    break;
+                case 'manual-shortcut-9':
+                    removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlLeft, ValidKey.Digit9], evalCode));
+                    removeShortcuts[agent.name].removeListener.push(registerShortcut([ValidKey.ControlRight, ValidKey.Digit9], evalCode));
+                    break;
+            }
+        });
+    });
+};
 
 chrome.storage.onChanged.addListener((changes, areaName) => {
     if (areaName !== 'local') return;
 
     console.log('changes:', changes);
+    InsertPageAgents();
 });
+
+InsertPageAgents();
