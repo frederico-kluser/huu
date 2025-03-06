6a0e32a00ec9a2566250e300eddda7203032f612
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2025-03-06 13:16:10 -0300

    feat: Melhoria na validação de agentes
    Aprimoramos a validação dos agentes em nossa aplicação. Agora, além de verificar se os agentes estão aprovados, também verificamos se as URLs correspondem ao padrão esperado. Além disso, fizemos algumas mudanças no nome das variáveis para melhorar a legibilidade do código e adicionamos um novo estado para armazenar a URL atual.

--- src/pages/Popup/pages/MainPage/index.tsx
+++ src/pages/Popup/pages/MainPage/index.tsx
@@ -4,6 +4,7 @@ import { TypeMode } from '../../../../types/agent';
 import Colors from '../../../../types/colors';
 import TypePageStyle from '../../../../types/pageStyle';
 import { fetchAgentById, updateAgentPartial } from '../../../../core/storage/agents';
+import urlMatchesPattern from '../../../../helpers/urlMatchePattern';
 
 interface MainPageProps {
     setIsMainPage: Dispatch<SetStateAction<boolean>>
@@ -11,8 +12,18 @@ interface MainPageProps {
 }
 
 const MainPage = ({ setIsMainPage, workspaces }: MainPageProps) => {
-    const [approvedAgents, setApprovedAgents] = useState<string[]>([]);
-    const [approvedAgentElements, setApprovedAgentElements] = useState<JSX.Element[]>([]);
+    const [validatedAgents, setValidatedAgents] = useState<string[]>([]);
+    const [agentItems, setAgentItems] = useState<JSX.Element[]>([]);
+    const [url, setUrl] = useState<string>('');
+
+    useEffect(() => {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            // tabs[0] contém informações sobre a aba ativa
+            var currentTab = tabs[0];
+            // Exibir a URL no elemento com id "url"
+            setUrl(currentTab.url || 'url não encontrada');
+        });
+    }, []);
 
     const updateApprovedAgents = async () => {
         const localApprovedAgents: string[] = [];
@@ -25,7 +36,7 @@ const MainPage = ({ setIsMainPage, workspaces }: MainPageProps) => {
             }
         }
 
-        setApprovedAgents(localApprovedAgents);
+        setValidatedAgents(localApprovedAgents);
     }
 
     useEffect(() => {
@@ -58,53 +69,55 @@ const MainPage = ({ setIsMainPage, workspaces }: MainPageProps) => {
     const generateAgentElements = async () => {
         const localApprovedAgentElements: JSX.Element[] = [];
 
-        for (let i = 0; i < approvedAgents.length; i++) {
-            const agent = approvedAgents[i];
+        for (let i = 0; i < validatedAgents.length; i++) {
+            const agent = validatedAgents[i];
             const retrievedAgent = await fetchAgentById(agent);
             const mode = retrievedAgent?.mode;
 
-            localApprovedAgentElements.push(
-                <div role="group" key={agent} style={styles.agentItem}>
-                    {await getButton(agent)}
-                    <h6 style={styles.agentTitle}>{agent}</h6>
-                    <select onChange={(e) => handleEditMode(agent, e.target.value as TypeMode)} value={mode}>
-                        <option value="">Selecione o modo de acionamento</option>
-                        <option value="automatic-1">Acionar automaticamente uma vez</option>
-                        <option value="automatic">Acionar automaticamente sem parar</option>
-                        <option value="manual-shortcut-2">Manual pelo atalho Ctrl + 2</option>
-                        <option value="manual-shortcut-3">Manual pelo atalho Ctrl + 3</option>
-                        <option value="manual-shortcut-4">Manual pelo atalho Ctrl + 4</option>
-                        <option value="manual-shortcut-5">Manual pelo atalho Ctrl + 5</option>
-                        <option value="manual-shortcut-6">Manual pelo atalho Ctrl + 6</option>
-                        <option value="manual-shortcut-7">Manual pelo atalho Ctrl + 7</option>
-                        <option value="manual-shortcut-8">Manual pelo atalho Ctrl + 8</option>
-                        <option value="manual-shortcut-9">Manual pelo atalho Ctrl + 9</option>
-                    </select>
-                </div>
-            );
+            if (urlMatchesPattern(url, retrievedAgent?.urls || '')) {
+                localApprovedAgentElements.push(
+                    <div role="group" key={agent} style={styles.agentItem}>
+                        {await getButton(agent)}
+                        <h6 style={styles.agentTitle}>{agent}</h6>
+                        <select onChange={(e) => handleEditMode(agent, e.target.value as TypeMode)} value={mode}>
+                            <option value="">Selecione o modo de acionamento</option>
+                            <option value="automatic-1">Acionar automaticamente uma vez</option>
+                            <option value="automatic">Acionar automaticamente sem parar</option>
+                            <option value="manual-shortcut-2">Manual pelo atalho Ctrl + 2</option>
+                            <option value="manual-shortcut-3">Manual pelo atalho Ctrl + 3</option>
+                            <option value="manual-shortcut-4">Manual pelo atalho Ctrl + 4</option>
+                            <option value="manual-shortcut-5">Manual pelo atalho Ctrl + 5</option>
+                            <option value="manual-shortcut-6">Manual pelo atalho Ctrl + 6</option>
+                            <option value="manual-shortcut-7">Manual pelo atalho Ctrl + 7</option>
+                            <option value="manual-shortcut-8">Manual pelo atalho Ctrl + 8</option>
+                            <option value="manual-shortcut-9">Manual pelo atalho Ctrl + 9</option>
+                        </select>
+                    </div>
+                );
+            }
         }
 
-        setApprovedAgentElements(localApprovedAgentElements);
+        setAgentItems(localApprovedAgentElements);
     };
 
     useEffect(() => {
         generateAgentElements()
-    }, [approvedAgents]);
+    }, [validatedAgents]);
 
     const handleEditModels = () => {
         setIsMainPage(false);
     };
 
     return (
-        <>
+        <div style={styles.container}>
             <h1>huu</h1>
-            {approvedAgents.length === 0 && <h4>Nenhum agente criado</h4>}
-            {approvedAgents.length > 0 && <h4>Agentes para este site <mark><i>youtube.com/</i></mark></h4>}
+            {validatedAgents.length === 0 && <h4>Nenhum agente criado</h4>}
+            {validatedAgents.length > 0 && <p>Agentes para este site<br /><mark><i>{url}</i></mark></p>}
             <div style={styles.agentContainer}>
-                {approvedAgentElements}
+                {agentItems}
             </div>
             <button onClick={handleEditModels}>Editar Agentes</button>
-        </>
+        </div>
     )
 };
 
@@ -135,6 +148,13 @@ const styles: TypePageStyle = {
         lineHeight: '56px',
         width: '75%',
     },
+    container: {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
 }
 
 export default MainPage;
