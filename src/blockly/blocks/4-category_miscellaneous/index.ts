c5e4b547947fb7fea2d056bd38aba3cf884cfab2
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2025-03-08 14:49:59 -0300

    feat: Remoção do bloco RunJavaScript
    O bloco RunJavaScript foi removido da configuração de ações diversas. Isso simplifica o código e evita possíveis problemas de segurança relacionados à execução de JavaScript.

--- src/blockly/blocks/4-category_miscellaneous/index.ts
+++ src/blockly/blocks/4-category_miscellaneous/index.ts
@@ -3,12 +3,10 @@ import setBlockAlertUser from "./blocks/BlockAlertUser";
 import setBlockConfirmUser from "./blocks/BlockConfirmUser";
 import setBlockDelayWait from "./blocks/BlockDelayWait";
 import setBlockPromptUser from "./blocks/BlockPromptUser";
-import setBlockRunJavaScript from "./blocks/BlockRunJavaScript";
 
 const configMiscellaneousActions = (): TypeBlockly[] => {
     return [
         setBlockDelayWait(),
-        // setBlockRunJavaScript(),
         setBlockAlertUser(),
         setBlockConfirmUser(),
         setBlockPromptUser(),
